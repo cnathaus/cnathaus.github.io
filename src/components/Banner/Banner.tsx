65e4ed125718f15f7c67e7d018dc21ece859c974
Christian Nathaus <christian.nathaus@tum.de>
2022-11-18 01:17:43 +0100

    changed banner text

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -10,7 +10,7 @@ export const Banner = () => {
   const [text, setText] = useState("");
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = ["piece of shit", "lukas hater"];
+  const toRotate = ["roboticist 🤖", "placeholder", "placeholder"];
   const period = 500;
 
   useEffect(() => {
