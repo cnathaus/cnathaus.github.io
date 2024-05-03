88b3ea21af93e7c3571d949a94a15814944bcaf5
cnathaus <christian.nathaus@tum.de>
2024-05-03 21:02:41 +0200

    fix deploy

--- app/brainlab/page.tsx
+++ app/brainlab/page.tsx
@@ -4,10 +4,15 @@ import { Header } from '../../components/Header/Header';
 import { Welcome } from '../../components/Welcome/Welcome';
 import { ProjectsCarousel } from '@/components/ProjectsCarousel/ProjectsCarousel';
 
+import { companies } from '@/assets/data/companies';
+
 export default function HomePage() {
+  const company = companies[0]; // TODO: modify so different companies can be displayed
+  const companyName = company.name;
+
   return (
     <>
-      <Header />
+      <Header companyName={companyName} />
       <Welcome />
 
       <ProjectsCarousel />
