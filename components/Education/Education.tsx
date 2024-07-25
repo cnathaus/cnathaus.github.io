2a8cc081e313d16ebc6406ce0c8f31a191f59e55
cnathaus <christian.nathaus@tum.de>
2024-07-25 21:10:29 +0200

    edit formatting

--- components/Education/Education.tsx
+++ components/Education/Education.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Button, List, Container, Title, Text, Timeline, Stack } from '@mantine/core';
+import { Avatar, Grid, List, Container, Title, Text, Timeline, Stack } from '@mantine/core';
 import { GradientOutlineButton } from '@/components/GradientOutlineButton/GradientOutlineButton';
 import { categoryColorMap, education } from '@/assets/data/education';
 import classes from './Education.module.scss';
@@ -40,74 +40,79 @@ function timelineAvatar({ src, imageProps }: TimeLineAvatarProps) {
 export function Education() {
   return (
     <Container id="education" size="md">
-      <Stack justify="center" gap={0} align="center">
-        <Title
-          order={2}
-          className={classes.title}
-          style={{
-            textAlign: 'center',
-            color: 'var(--mantine-color-default-color)',
-            marginTop: '3rem',
-            marginBottom: '1.5rem',
-          }}
-        >
-          Education
-        </Title>
+      <Grid justify="center" align="center" gutter={60}>
+        <Grid.Col span={10}>
+          <Stack justify="center" gap={0} align="center">
+            <Title
+              order={2}
+              className={classes.title}
+              style={{
+                textAlign: 'center',
+                color: 'var(--mantine-color-default-color)',
+                marginTop: '3rem',
+                marginBottom: '1.5rem',
+              }}
+            >
+              Education
+            </Title>
 
-        <Text
-          style={{ textAlign: 'center', marginBottom: '2rem' }}
-          fw={500}
-          c="light-dark(var(--mantine-color-gray-7), var(--mantine-color-dark-0))"
-        >
-          I graduated with a bachelor&apos;s degree in mechanical engineering from RWTH Aachen.
-          While spending a transformative semester abroad at NUS in Singapore, I discovered my
-          passion for robotics and software development. This led me to pursue a master&apos;s
-          degree in robotics and AI at TUM, where I focused on human-robot collaboration. My goal
-          now is to apply my passion and expertise to make meaningful contributions in the fields of
-          medical or industrial robotics, aiming to make a positive impact on people&apos;s lives.
-        </Text>
-        <Timeline
-          bulletSize={bulletSize}
-          reverseActive
-          style={{ justifyContent: 'center', marginBottom: '3rem' }}
-        >
-          {education.map((item, index) => {
-            const color = categoryColorMap[item.category];
-            return (
-              <Timeline.Item
-                title={item.title}
-                // lineVariant={index === 0 ? 'dotted' : 'solid'}
-                bullet={item.logo ? timelineAvatar(item.logo) : null}
-                classNames={{ item: classes.item }}
-                styles={{
-                  itemBody: {
-                    marginTop: '0.75rem', // TODO: replace with calculated value
-                    // alignItems: 'center',
-                  },
-                  itemBullet: {
-                    borderColor: color,
-                  },
-                }}
-              >
-                <Text c="dimmed" size="sm">
-                  {item.subtitle}
-                </Text>
-                <Text c="dimmed" size="sm">
-                  {item.date}
-                </Text>
-                {item.details && (
-                  <List c="dimmed" size="sm">
-                    {item.details.map((detail) => (
-                      <List.Item key={detail}>{detail}</List.Item>
-                    ))}
-                  </List>
-                )}
-              </Timeline.Item>
-            );
-          })}
-        </Timeline>
-        <GradientOutlineButton text="Check out my CV" onClick={downloadCV} />
-      </Stack>
+            <Text
+              style={{ textAlign: 'justify', marginBottom: '2rem' }}
+              fw={500}
+              c="light-dark(var(--mantine-color-gray-7), var(--mantine-color-dark-0))"
+            >
+              I graduated with a bachelor&apos;s degree in mechanical engineering from RWTH Aachen.
+              While spending a transformative semester abroad at NUS in Singapore, I discovered my
+              passion for robotics and software development. This led me to pursue a master&apos;s
+              degree in robotics and AI at TUM, where I focused on human-robot collaboration. My
+              goal now is to apply my passion and expertise to make meaningful contributions in the
+              fields of medical or industrial robotics, aiming to make a positive impact on
+              people&apos;s lives.
+            </Text>
+            <Timeline
+              bulletSize={bulletSize}
+              reverseActive
+              style={{ justifyContent: 'center', marginBottom: '3rem' }}
+            >
+              {education.map((item, index) => {
+                const color = categoryColorMap[item.category];
+                return (
+                  <Timeline.Item
+                    title={item.title}
+                    // lineVariant={index === 0 ? 'dotted' : 'solid'}
+                    bullet={item.logo ? timelineAvatar(item.logo) : null}
+                    classNames={{ item: classes.item }}
+                    styles={{
+                      itemBody: {
+                        marginTop: '0.75rem', // TODO: replace with calculated value
+                        // alignItems: 'center',
+                      },
+                      itemBullet: {
+                        borderColor: color,
+                      },
+                    }}
+                  >
+                    <Text c="dimmed" size="sm">
+                      {item.subtitle}
+                    </Text>
+                    <Text c="dimmed" size="sm">
+                      {item.date}
+                    </Text>
+                    {item.details && (
+                      <List c="dimmed" size="sm">
+                        {item.details.map((detail) => (
+                          <List.Item key={detail}>{detail}</List.Item>
+                        ))}
+                      </List>
+                    )}
+                  </Timeline.Item>
+                );
+              })}
+            </Timeline>
+            <GradientOutlineButton text="Check out my CV" onClick={downloadCV} />
+          </Stack>
+        </Grid.Col>
+      </Grid>
     </Container>
   );
 }
--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -14,13 +14,13 @@ function downloadCV() {
 export function Welcome() {
   return (
     <Container id="about" size="lg">
-      <Grid justify="center" align="center" mt={75} gutter={60}>
+      <Grid justify="center" align="center" gutter={60}>
         <Grid.Col span="content" order={{ base: 2, md: 1, lg: 1 }}>
           <Image radius="md" h={400} src={img.src} fit="contain" />
           {/* <Image radius="md" h={400} src={img2.src} fit="contain" />
           <Image radius="md" h={400} src={img3.src} fit="contain" /> */}
         </Grid.Col>
-        <Grid.Col span={{ base: 12, md: 6, lg: 6 }} order={{ base: 1, md: 2, lg: 2 }}>
+        <Grid.Col span={{ base: 10, md: 6, lg: 6 }} order={{ base: 1, md: 2, lg: 2 }}>
           <Title
             className={classes.title}
             // ta="center"
@@ -43,7 +43,7 @@ export function Welcome() {
             fw={500}
             c="light-dark(var(--mantine-color-gray-7), var(--mantine-color-dark-0))"
             mb={50}
-            style={{ textAlign: 'center' }}
+            style={{ textAlign: 'justify' }}
           >
             {' '}
             I&apos;m passionate about enhancing human-robot interactions and leveraging AI methods
