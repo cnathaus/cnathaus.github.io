7f7cb3e5f13b02ccaa5a05a15e857a807b97c86f
cnathaus <christian.nathaus@tum.de>
2024-08-21 15:51:27 +0200

    add ga

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@mantine/core/styles.css';
 import '@mantine/carousel/styles.css';
 import React from 'react';
+import { GoogleAnalytics } from '@next/third-parties/google';
 import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 import { ModalsProvider } from '@mantine/modals';
 import { theme } from '../theme';
@@ -13,6 +14,7 @@ export default function RootLayout({ children }: { children: any }) {
   return (
     <html lang="en">
       <head>
+        <GoogleAnalytics gaId="G-X8MK3HFW5P" />
         <ColorSchemeScript />
         <link rel="shortcut icon" href="/logo192.png" />
         <meta
